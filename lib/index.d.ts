ac055f96dd3ca3a38b80ee01ed6e38ac5d8b573d
cjslack <40322960+cjslack@users.noreply.github.com>
2020-10-22 15:51:40 -0400

    Init

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -10,7 +10,3 @@ export class GrokCollection {
   loadSync(filePath: string): number;
   count(): number;
 }
-
-export function loadDefault(loadModulesOrCallback?: string[] | string | { (err: any, collection: GrokCollection): any }, callback?: { (err: any, collection: GrokCollection): any }): any;
-
-export function loadDefaultSync(loadModules?: string[] | string): GrokCollection;
